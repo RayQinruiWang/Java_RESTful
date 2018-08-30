f6bbeeeab9cc926e0aa0c31d462e6ae52ffc1596
Ray.Wang@Oakton.com.au <Ray.Wang@Oakton.com.au>
2018-08-30 16:31:04 +1000

    JSON format refined, added owner notation and JSONArray

--- src/todoServices/GetTodoService.java
+++ src/todoServices/GetTodoService.java
@@ -11,6 +11,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 @Path("/gettodo")
@@ -20,15 +21,25 @@ public class GetTodoService {
 	public String retrieveToDo(@QueryParam("owner") String owner) {
 		JSONObject result = new JSONObject();
 		
+		
 		try {
 			DriverManager.registerDriver(new com.mysql.jdbc.Driver ());
 			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/to-do_users?useSSL=false","root","admin");
 			Statement myStmt = myConn.createStatement();
 			ResultSet myRs = myStmt.executeQuery("select * from todos where owner = '" + owner +"'");
 			
+			JSONArray todos = new JSONArray();
+			
 			while (myRs.next()) {
-				result.put(myRs.getString("description"), myRs.getString("category"));
+				// construct the JSON object for each todo's
+				JSONObject todo = new JSONObject();
+				todo.put("description", myRs.getString("description"));
+				todo.put("category", myRs.getString("category"));
+				todos.put(todo);
 			}
+			
+			result.put("owner", owner);
+			result.put("todos", todos);
 		}
 		catch(Exception exc) {
 			exc.printStackTrace();
